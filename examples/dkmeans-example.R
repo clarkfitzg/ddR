2d2cca72750277e081f741ee4530180b75917194
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-08-16 16:45:21 +0900

    comments explaining kmeans example

--- examples/dkmeans-example.R
+++ examples/dkmeans-example.R
@@ -3,27 +3,33 @@ library(kmeans.ddR)
 
 set.seed(319804)
 
-N_EXEC = 2 # Change level of parallelism
-useBackend(parallel, executors = N_EXEC)
-# Uncomment the following lines to use Distributed R 
-#library(distributedR.ddR)
-#useBackend(distributedR)
+# Define level of parallelism
+N_EXEC = 2 
 
 # Set up data size
 NCOL = 8 # Must be at least 2
-NROW = as.integer(2e5)
+NROW = 2e5L
 K = 6
 ITERMAX = 300
 
+# Set the backend
+useBackend(parallel, executors = N_EXEC)
+
+# Or uncomment the following lines to use Distributed R 
+#library(distributedR.ddR)
+#useBackend(distributedR)
+
 # The true centers for the data generating process
-#centers = matrix(rnorm(K * NCOL, sd = 10), nrow = K)
 # Easy to check correctness if we know where these centers should be
 centers = cbind(10 * seq.int(K), matrix(0, nrow = K, ncol = NCOL - 1))
 dnumrows = as.integer(NROW/N_EXEC)
 
-generateKMeansData <- function(id, centers, nrow, ncol) {
+# This function will run on the distributed backend.
+# Parallelism is generally easier if this is a "pure" function,
+# meaning it doesn't depend on global state.
+generateKMeansData <- function(id, centers, nrow, ncol, k) {
     offsets = matrix(rnorm(nrow * ncol), nrow = nrow, ncol = ncol)
-    cluster_ids = sample.int(K, size = nrow, replace = TRUE)
+    cluster_ids = sample.int(k, size = nrow, replace = TRUE)
     feature_obs = centers[cluster_ids, ] + offsets
     feature_obs
 }
@@ -32,17 +38,22 @@ cat(sprintf("Generating %d x %d matrix for clustering with %d means\n",
             NROW, NCOL, K))
 
 dfeature <- dmapply(generateKMeansData, id = 1:N_EXEC,
-  MoreArgs = list(centers = centers, nrow = dnumrows, ncol = NCOL),
+  MoreArgs = list(centers = centers, nrow = dnumrows, ncol = NCOL, k = K),
 		output.type = "darray", 
 		combine = "rbind", nparts = c(N_EXEC,1))
 
+# Parallelism has overhead, and depending on the problem this may or
+# may not be faster than a single threaded version. But generally parallel
+# algorithms scale better to larger data.
 cat("training dkmeans model on distributed data\n")
 dtraining_time <- system.time(
     dmodel <- dkmeans(dfeature, K, iter.max = ITERMAX)
 )[3]
 cat(dtraining_time, "\n")
 
+# Collecting makes the distributed object into a local object
 feature <- collect(dfeature)
+
 cat("training kmeans model on centralized data\n")
 training_time <- system.time(
     model <- kmeans(feature, K, iter.max = ITERMAX, algorithm = "Lloyd")
@@ -52,9 +63,16 @@ cat(training_time, "\n")
 # Compare results
 sortfirstcol = function(m) m[order(m[, 1]), ]
 
+# A list of three matrices with the coordinates of the cluster centers
+# represented as rows
 allcenters = lapply(list(true = centers, 
                          distributed = dmodel$centers,
                          centralized = model$centers),
                     sortfirstcol)
 
 print(allcenters)
+
+# Shutdown the backend because distributed computation is finished
+shutdown(parallel)
+
+plot(
