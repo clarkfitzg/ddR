4609505bc9398f62cf3661a86fd1f498185189ac
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-09-02 10:28:25 +0900

    attempting direct port of parallel objects

--- sparkapi.ddR/R/driver.R
+++ sparkapi.ddR/R/driver.R
@@ -31,9 +31,9 @@ init_spark <- function(master = "local", ...) {
     executors = length(memory_status)
 
     new("Spark.ddR",
-        DListClass = "ddR_RDD",
-        DFrameClass = "ddR_RDD",
-        DArrayClass = "ddR_RDD",
+        DListClass = "ParallelObj",
+        DFrameClass = "ParallelObj",
+        DArrayClass = "ParallelObj",
         name = "spark",
         executors = executors,
         sc = sc
@@ -60,7 +60,7 @@ Rlist_to_dlist = function(Rlist, nparts){
 
     rdd = rddlist(sparkapi.ddR.env$sc, partlist)
 
-    new("ddR_RDD", RDD = rdd, nparts = nparts, psize = psizes, dim = dims, 
+    new("ParallelObj", RDD = rdd, nparts = nparts, psize = psizes, dim = dims, 
         partitions = 1:prod(nparts))
 
 }
@@ -81,26 +81,196 @@ function(driver, func, ..., MoreArgs = list(),
     ## 4: Insert wrapper functions, list-of-parts conversion
     ## 5: Run lapplyWithPartitions
     ## 6: Collect and compute psizes
-    ## 7: Create new ddR_RDD object
+    ## 7: Create new ParallelObj object
 
 
-    dots = list(...)
-    rdds = lapply(dots, rddlist, sc = driver@sc)
-    mapply_args = c(list(func), rdds)
-    output_rdd = do.call(mapply_rdd, mapply_args)
+#    dots = list(...)
+#    rdds = lapply(dots, rddlist, sc = driver@sc)
+#    mapply_args = c(list(func), rdds)
+#    output_rdd = do.call(mapply_rdd, mapply_args)
+#
+#    output_length = length_rdd(output_rdd)
+#
+#    # TODO: temporarily hardcoding all this in to get simple thing working.
+#    # This assumes a list with the same number of partitions as it's length,
+#    # like an rddlist
+#    if(output.type == "dlist"){
+#        dims = output_length
+#        psizes = matrix(rep(1L, output_length), ncol=1L)
+#        nparts = c(output_length, 1L)
+#    }
+#
+#    new("ParallelObj", RDD = output_rdd, nparts = nparts, psize = psizes, dim = dims, 
+#        partitions = 1:prod(nparts))
 
-    output_length = length_rdd(output_rdd)
+# Clark: Why is this code here rather than in the dmapply function?
+  stopifnot(is.list(MoreArgs))
+  output.type <- match.arg(output.type)
+  if (!is.null(nparts)) {
+      stopifnot(is.numeric(nparts),
+                length(nparts) == 1L || length(nparts) == 2L)
+  }
+  combine <- match.arg(combine)
 
-    # TODO: temporarily hardcoding all this in to get simple thing working.
-    # This assumes a list with the same number of partitions as it's length,
-    # like an rddlist
-    if(output.type == "dlist"){
-        dims = output_length
-        psizes = matrix(rep(1L, output_length), ncol=1L)
-        nparts = c(output_length, 1L)
+  dots <- list(...)
+  dlen<-length(dots)
+
+
+  for(num in 1:dlen){
+    if(is(dots[[num]],"DObject")){
+      #If this is a DObject, we need to extract the backend object and reassemble it (i.e., use collect)
+      #By converting DObject into a normal R object, we let mcmapply handle how iterations on differnt data types
+      dots[[num]] <- collect(dots[[num]])
+    }else{
+      #There are two cases for a list (1) parts(dobj) or (2) list of parts(dobj)
+
+      if(is(dots[[num]],"list") && any(rapply(dots[[num]], function(x) is(x,"ParallelObj"),how="unlist"))){
+        tmp <- rapply(dots[[num]],function(argument){
+	    	      if(is(argument,"DObject"))
+            	         return(argument@pObj[argument@splits])
+	              else return(argument)}, how="replace")
+
+       #(iR): This is bit of a hack. rapply increases the depth of the list, but at the level that the replacement occured.
+       #Simple ulist() does not work. If this was just parts(A,..), we can call unlist. If this is a list of parts, then
+       #we unwrap the second layer of the list. Unwrapping the second layer is incorrect if parts(A) was embedded deeper than that.
+       if(is(dots[[num]][[1]], "DObject")){
+       	  dots[[num]] <- unlist(tmp, recursive=FALSE)
+       } else {
+          for(index in seq_along(tmp)){
+       	        if(length(tmp[[index]])>0)
+			tmp[[index]] <- unlist(tmp[[index]], recursive=FALSE)
+	        else
+			tmp[[index]] <- tmp[[index]]
+ 	       }
+          dots[[num]] <- tmp
+      }
+     }
     }
+   }
 
-    new("ddR_RDD", RDD = output_rdd, nparts = nparts, psize = psizes, dim = dims, 
-        partitions = 1:prod(nparts))
+   #Check if MoreArgs contains a distributed object. If yes, convert it into a regular object via collect
+   for(index in seq_along(MoreArgs)){
+         if(is(MoreArgs[[index]], "DObject"))
+      	   MoreArgs[[index]] <- collect(MoreArgs[[index]])
+   }
+
+    answer <- NULL
+    # Now iterate in parallel
+
+############################################################
+
+# Original code with rddlist:
+#    dots = list(...)
+#    rdds = lapply(dots, rddlist, sc = driver@sc)
+#    mapply_args = c(list(func), rdds)
+#    output_rdd = do.call(mapply_rdd, mapply_args)
+
+# Parallel code:
+#    # Wrap the input arguments and use do.call()
+#    allargs <- c(list(cl = driver@cluster,
+#                      fun = func,
+#                      MoreArgs = MoreArgs,
+#                      RECYCLE = FALSE, SIMPLIFY = FALSE),
+#                 dots)
+#    answer <- do.call(parallel::clusterMap, allargs)
+
+# New code
+# TODO: support MoreArgs
+rdds <- lapply(dots, rddlist, sc = driver@sc)
+mapply_args <- c(list(func), rdds)
+output_rdd <- do.call(mapply_rdd, mapply_args)
+answer <- collect_rdd(output_rdd)
+
+############################################################
+
+   #Perform a cheap check on whether there was an error since man pages say that an error on one core will result in error messages on all. TODO: Sometimes the class of the error is "character"
+   if(inherits(answer[[1]], "try-error")) {stop(answer[[1]])}
+   if(class(answer[[1]]) == "character" && grepl("Error", answer[[1]])) {stop(answer[[1]])}
+
+   #Create the output object, since we store partitions
+   totalParts <- prod(nparts)
+   outputObj<-vector("list", totalParts)
+
+   #Create the output based on the number of partitions expected in the output
+   #We create partitions with near equal sizes, i.e. if the #elements is not purely divisible by totalparts, we spread the remainder
+   #to the 1:remainder elements
+   lenAnswer<-length(answer)
+   if(totalParts > lenAnswer) stop("Number of elements generated by dmapply is less than those supplied in argument 'nparts'.")
+
+   remainder<-lenAnswer %% totalParts
+   elemInEachPart<- rep(floor(lenAnswer/totalParts), totalParts)
+   if(remainder>0) {elemInEachPart[1:remainder]<-elemInEachPart[1:remainder]+1}
+   elemInEachPart<-c(0,cumsum(elemInEachPart))
+
+   #Decide how we may need to combine entries from the answer into partitions
+   #We handle the "flatten" case in the while loop since simplify2array has to be called with parameter "higher=FALSE"
+   combineFunc <- list
+
+   if(output.type !="dlist"){
+       #Setup the partition types that we will use later to check if partitions conform to output.type.
+       if(output.type == "darray") ptype<-"matrix"
+       if(output.type == "dframe") ptype<-"data.frame"
+       if(output.type == "sparse_darray") ptype<-c("dsCMatrix", "dgCMatrix")
+
+	if(combine == "rbind"){
+	   if(ddR.env$RminorVersion > 2) #If R >3.2, use new rbind
+	   	   combineFunc <- rbind
+           else
+	   	   combineFunc <- Matrix::rBind
+	}
+	else if(combine == "cbind"){
+	   if(ddR.env$RminorVersion > 2) #If R >3.2, use new cbind
+	   	   combineFunc <- cbind
+           else
+	   	   combineFunc <- Matrix::cBind
+       }
+   }
+   index<-1
+   psizes<-array(0L,dim=c(totalParts,2)) #Stores partition sizes
+   while(index <= totalParts){
+   	     if(output.type == "dlist"){
+	        if(combine == "c")
+			     outputObj[[index]] <- unlist(answer[(elemInEachPart[index]+1):elemInEachPart[index+1]], recursive=FALSE)
+	        else
+			     outputObj[[index]] <- answer[(elemInEachPart[index]+1):elemInEachPart[index+1]]
+             }else {
+	        if(combine == "c" || combine =="default"){
+	        	     outputObj[[index]] <- simplify2array(answer[(elemInEachPart[index]+1):elemInEachPart[index+1]], higher=FALSE)
+                }else{
+			     outputObj[[index]] <- do.call(combineFunc, answer[(elemInEachPart[index]+1):elemInEachPart[index+1]])
+                }
+	        if(!(class(outputObj[[index]]) %in% ptype)) {stop("Each partition of the result should be of type = ", ptype, ", to match with output.type =", output.type)}
+	     }
+
+	     d<-dim(outputObj[[index]])
+	     psizes[index,] <-(if(is.null(d)){c(length(outputObj[[index]]),0L)} else {d})
+	     index<-index+1
+   }
+
+   dims<-NULL
+
+   #Check if partions conform and can be stitched together.
+   #Check partitions in each logical row have the same number of rows/height. Similary for columns
+   rowseq<-seq(1, totalParts, by=nparts[2])
+   for (index in rowseq){
+	     if(any(psizes[index:(index+nparts[2]-1),1]!=psizes[index,1])) stop("Adjacent partitions have different number of rows, should be ", psizes[index,1])
+   }
+
+   for (index in 1:nparts[2]){
+	    if(any(psizes[(rowseq+(index-1)),2]!=psizes[index,2])) stop("Adjacent partitions have different number of columns, should be ", psizes[index,2])
+   }
+
+  numcols<-sum(psizes[1:nparts[2],2]) #add cols of all partitions in the first row
+  numrows<-sum(psizes[seq(1, totalParts, by=nparts[2]), 1]) #add all partitions in the first column group
+  dims<-c(numrows, numcols)
+
+
+  #Use single dimension if we know it's a list
+  if(output.type=="dlist"){
+	 psizes<-as.matrix(psizes[,1])
+	 dims<-dims[1]
+   }
+
+   new("ParallelObj",pObj = outputObj, splits = 1:length(outputObj), psize = psizes, dim = as.integer(dims), nparts = nparts)
 
 })
---
+++ sparkapi.ddR/R/sparklite.R
@@ -0,0 +1,391 @@
+build_parent_env <- function(varlist, envir){
+    out <- new.env
+    for(name in varlist){
+        assign(name, get(name, envir), out)
+    }
+    out
+}
+
+
+#' Create or update a function closure with new values
+#' 
+#' Adds data to a function so that it can be serialized and run on a remote
+#' cluster, avoiding errors of type \code{Object not found}.
+#'
+#' The updating behavior means that if \code{fun} already has an
+#' environment containing \code{a = 10} then running \code{a <- 20;
+#' updateClosure(fun, "a")} will return a function where \code{a = 20}.
+#' 
+#' This function is modeled after \code{\link[parallel]{clusterExport}}.
+#'
+#' @param fun function with old or no environment
+#' @param varlist character vector of names of objects to export.
+#' @param envir environment from which to export variables
+#' @return function with updated environment
+#' @seealso \code{\link[parallel]{clusterExport}}
+#'
+#' @examples
+#' # To apply f you'll need to also get it's dependencies
+#' f <- makeClosure(f, c("a", "helperfunc"))
+#' clusterApply(sc, 1:10, f)
+#' @export
+update_closure <- function(fun, varlist, envir = .GlobalEnv){
+}
+
+
+#' Parallelize computations using a Spark cluster
+#'
+#' This works by serializing x onto the worker nodes, running the
+#' computation, and finally deserializing the result.
+#' 
+#' @param cl cluster is a Spark connection as returned from
+#'      \code{\link[sparkapi]{start_shell}}
+#' @param x R object that can be coerced to list
+#' @param fun function to evaluate
+#'
+#' @return list with \code{fun} evaluated at each element of x
+#'
+#' @examples
+#' library(sparkapi)
+#' sc <- start_shell(master = "local")
+#'
+#' clusterApply(sc, 1:10, function(x) x + 2)
+#'
+#' a <- 20
+#' helperfunc <- function(x) sin(x)
+#' f <- function(x) helperfunc(x) + a
+#' # To apply f you'll need to also get it's dependencies
+#' f <- makeClosure(f, c("a", "helperfunc"))
+#' clusterApply(sc, 1:10, f)
+#'
+#' @seealso \code{makeClosure}, \code{\link[base]{lapply}}, 
+#'      \code{\link[parallel]{clusterApply}},
+#'      \code{\link[parallel]{clusterExport}}, in \code{parallel} package
+#' @export
+clusterApply <- function(cl, x, fun, ...){
+
+    sc <- cl
+    x <- as.list(x)
+    fun <- match.fun(fun)
+
+    sparkfun <- function(partIndex, part) {
+        fun(part)
+    }
+
+    packageNamesArr <- serialize(NULL, connection = NULL)
+
+    xserial <- lapply(x, serialize, connection = NULL)
+
+    # An RDD of the serialized R parts
+    # This is class org.apache.spark.api.java.JavaRDD
+    xrdd <- sparkapi::invoke_static(sc,
+                "org.apache.spark.api.r.RRDD",
+                "createRDDFromArray",
+                sparkapi::java_context(sc),
+                xserial
+                )
+
+    # Use Spark to apply the function
+    fxrdd <- sparkapi::invoke_new(sc,
+                "org.apache.spark.api.r.RRDD",  # A new instance of this class
+                sparkapi::invoke(xrdd, "rdd"),
+                serialize(sparkfun, connection = NULL),
+                "byte",     # name of serializer / deserializer
+                "byte",     # name of serializer / deserializer
+                packageNamesArr,
+                list(),     # broadcastArr
+                sparkapi::invoke(xrdd, "classTag")
+                )
+
+    # Collect and return
+    rawlist = sparkapi::invoke(fxrdd, "collect")
+    lapply(rawlist, unserialize)
+}
+
+
+# Create an rddlist from a local R list.
+# 
+# Each element of the local R list corresponds to an element of the Spark
+# RDD (Resilient Distributed Dataset)
+#
+# @param sc Spark connection as returned from
+#      \code{\link[sparkapi]{start_shell}}
+# @param X local R list.
+# @param cache logical - Should the resulting RDD be cached in Spark's
+#      memory?
+#
+# @return rddlist A Spark Java Object representing the rddlist
+#
+# @examples
+# x <- list(1:10, letters, rnorm(10))
+# xrdd <- rddlist(sc, x)
+#
+# @export
+rddlist <- function(sc, X, cache=TRUE){
+    if(!is.list(X)){
+        stop("X should be a list")
+    }
+
+    serial_parts = lapply(X, serialize, connection = NULL)
+
+    # An RDD of the serialized R parts
+    # This is class org.apache.spark.api.java.JavaRDD
+    RDD = invoke_static(sc,
+                        "org.apache.spark.api.r.RRDD",
+                        "createRDDFromArray",
+                        sparkapi::java_context(sc),
+                        serial_parts)
+
+    # (data, integer) pairs
+    backwards = invoke(RDD, "zipWithIndex")
+
+    # An RDD of integers
+    index = invoke(backwards, "values")
+
+    # The pairRDD of (integer, data) 
+    pairRDD = invoke(index, "zip", RDD)
+
+    # This is all written specifically for bytes, so should be fine to let this 
+    # classTag hang around
+    new_rddlist(pairRDD, invoke(RDD, "classTag"), cache)
+}
+
+
+# new rddlist
+#
+# Create an instance of rddlist as subclass of sparkapi spark_jobj
+#
+new_rddlist <- function(pairRDD, classTag, cache){
+    out <- pairRDD
+    attr(out, "classTag") <- classTag
+    class(out) <- c("rddlist", class(pairRDD))
+    if(cache) invoke(out, "cache")
+    out
+}
+
+
+# Apply a Function over an rddlist
+# 
+# Modeled after \code{lapply} in base R
+#
+# @param X rddlist
+# @param FUN function to apply to each element of X
+# @param cache logical - Should the resulting RDD be cached in Spark's
+#      memory?
+#
+# @return rddlist A Spark Java Object representing the resulting rddlist
+#
+# @examples
+# x <- list(1:10, letters, rnorm(10))
+# xrdd <- rddlist(sc, x)
+# lapply_rdd(xrdd, head)
+#
+# @export
+lapply_rdd <- function(X, FUN, cache=TRUE){
+# TODO: support dots function(X, FUN, ...){
+
+    # The function should be in a particular form for calling Spark's
+    # org.apache.spark.api.r.RRDD class constructor
+    FUN_applied = function(partIndex, part) {
+        FUN(part)
+    }
+    FUN_clean = cleanClosure(FUN_applied)
+
+    # TODO: Could come back and implement this functionality later
+    packageNamesArr <- serialize(NULL, NULL)
+    broadcastArr <- list()
+    # I believe broadcastArr holds these broadcast variables:
+    # https://spark.apache.org/docs/latest/programming-guide.html#broadcast-variables
+    # But what's the relation between broadcast variables, FUN's closure,
+    # and the ... argument?
+    
+    vals = invoke(X, "values")
+
+    # Use Spark to apply FUN
+    fxrdd <- invoke_new(sparkapi::spark_connection(X),
+                       "org.apache.spark.api.r.RRDD",  # A new instance of this class
+                       invoke(vals, "rdd"),
+                       serialize(FUN_clean, NULL),
+                       "byte",  # name of serializer / deserializer
+                       "byte",  # name of serializer / deserializer
+                       packageNamesArr,  
+                       broadcastArr,
+                       X$classTag
+                       )
+
+    # Convert this into class org.apache.spark.api.java.JavaRDD so we can
+    # zip
+    JavaRDD = invoke(fxrdd, "asJavaRDD")
+
+    # Reuse the old index to create the PairRDD
+    index = invoke(X, "keys")
+    pairRDD = invoke(index, "zip", JavaRDD)
+   
+    new_rddlist(pairRDD, X$classTag, cache)
+}
+
+
+# Collect the ith element of an rddlist
+# @export
+`[[.rddlist` <- function(x, i){
+    javaindex = as.integer(i - 1L)
+    javabytes = invoke(x, "lookup", javaindex)
+    # The bytes come wrapped in a list
+    bytes = invoke(javabytes, "toArray")[[1]]
+    unserialize(bytes)
+}
+
+
+# zip 2 rddlists into one
+#
+# This does the actual work for zip_rdd
+#
+# a_nested = TRUE means that a is already in the form of a nested list with
+# two layers: [ [a1], [a2], ... , [an] ]
+zip2 <- function(a, b, a_nested = FALSE, b_nested = FALSE){
+    # They must be nested for this to work
+    if(!a_nested){
+        a = lapply_rdd(a, list)
+    }
+    if(!b_nested){
+        b = lapply_rdd(b, list)
+    }
+    aval = invoke(a, "values")
+    bval = invoke(b, "values")
+    # class org.apache.spark.api.java.JavaPairRDD
+
+    zipped = invoke(aval, "zip", bval)
+    # class org.apache.spark.rdd.ZippedPartitionsRDD2
+    # This has the same number of elements as the input a.
+    # Converting to rdd seems necessary for the invoke_new below
+    RDD = invoke(zipped, "rdd")
+
+    partitionFunc <- function(partIndex, part) {
+        part
+    }
+    FUN_clean = cleanClosure(partitionFunc)
+    packageNamesArr <- serialize(NULL, NULL)
+    broadcastArr <- list()
+
+    pairs <- invoke_new(sparkapi::spark_connection(a),
+                       "org.apache.spark.api.r.RRDD",  # A new instance of this class
+                       RDD,
+                       serialize(FUN_clean, NULL),
+                       "byte",  # name of serializer / deserializer
+                       "byte",  # name of serializer / deserializer
+                       packageNamesArr,  
+                       broadcastArr,
+                       a$classTag
+                       )
+
+    JavaRDD = invoke(pairs, "asJavaRDD")
+    index = invoke(a, "keys")
+    pairRDD = invoke(index, "zip", JavaRDD)
+
+    new_rddlist(pairRDD, a$classTag, cache=FALSE)
+}
+
+
+# Zip rdds together
+# 
+# For rdds a, b, ... of the same length n this creates an rddlist of length n
+# where the ith element has the value list(a[[i]], b[[i]], ... )
+#
+# It always returns a nested list.
+#
+# @param ... rddlists of the same length
+# @param cache logical - Should the resulting RDD be cached in Spark's
+#      memory?
+#
+# @return rddlist A Spark Java Object representing the resulting rddlist
+zip_rdd <- function(..., cache=TRUE){
+    args = list(...)
+    a = args[[1]]
+    n = length(args)
+
+    zipped = lapply_rdd(a, list, cache = FALSE)
+
+    if(n == 1L){
+        # Easy out for trivial case
+        # Note zipping will always have the same nested structure
+        return(zipped)
+    }
+
+    # A 'reduce' operation
+    for(i in 2:n){
+        zipped = zip2(zipped, args[[i]], a_nested = TRUE)
+    }
+
+    # The idea with caching is to avoid it in the intermediate steps
+    # TODO: verify that this strategy is actually useful
+    if(cache) invoke(zipped, "cache")
+
+    zipped
+}
+
+
+# Apply a Function to multiple rddlist arguments
+# 
+# Modeled after \code{mapply} in base R
+#
+# @param FUN function to apply to each element of X
+# @param ... rddlists of the same length
+# @param cache logical - Should the resulting RDD be cached in Spark's
+#      memory?
+#
+# @return rddlist A Spark Java Object representing the resulting rddlist
+#
+# @examples
+# x <- rddlist(sc, list(1:10, letters, rnorm(10)))
+# y <- rddlist(sc, list(21:30, LETTERS, rnorm(10)))
+# xy <- mapply_rdd(c, x, y)
+#
+# @export
+mapply_rdd <- function(FUN, ..., cache = TRUE){
+
+    # TODO: add recycling, Moreargs
+    FUN = match.fun(FUN)
+    zipped = zip_rdd(..., cache = FALSE)
+    
+    # The parts in zipped are always lists
+    zipFUN = function(zipped_part){
+        do.call(FUN, zipped_part)
+    }
+
+    lapply_rdd(zipped, zipFUN, cache)
+}
+
+
+# length of rddlist
+# 
+# @param rdd rddlist
+#
+# @return n integer length 
+# @export
+length_rdd <- function(rdd){
+    # sparkapi maps Java long -> double
+    as.integer(invoke(rdd, "count"))
+}
+
+
+# Collect from Spark
+# 
+# Collects and unserializes the entire rdd from Spark back into local R.
+# 
+# @param rdd rddlist
+#
+# @return list The contents of rddlist in a local R list
+#
+# @examples
+# x <- list(1:10, letters, rnorm(10))
+# xrdd <- rddlist(sc, x)
+# x2 <- collect(xrdd)
+# identical(x, x2)
+# @export
+collect <- function(rdd){
+    values = invoke(rdd, "values")
+    collected = invoke(values, "collect")
+    rawlist = invoke(collected, "toArray")
+    lapply(rawlist, unserialize)
+}
+
