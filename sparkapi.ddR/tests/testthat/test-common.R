41d4172aed9fd6c1bedc41b42e875457735d404b
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-09-02 15:48:34 +0900

    setting up regular unit tests

--- sparkapi.ddR/tests/testthat/test-common.R
+++ sparkapi.ddR/tests/testthat/test-common.R
@@ -1,13 +1,21 @@
 # file for testing common functionality between ddR and another backend
+library(testthat)
+library(sparkapi.ddR)
 
 driver_name = "spark"
 
-useBackend(driver_name, warn = FALSE)
+useBackend(driver_name)
 
 context("testing common features")
 
 # Test everything except those specific to the parallel driver
-to_test <- list.files("../../../ddR/tests/testthat")
-to_test <- to_test[to_test != "test-pdriver.R"]
+# TODO: change this to a relative path
+to_test <- list.files("/Users/clark/dev/ddR/ddR/tests/testthat",
+                    full.names = TRUE)
+to_test <- to_test[!grepl("test-pdriver.R", to_test)
+                    & grepl("test.+\\.R", to_test)]
+
+# Some tests rely on this explicitly
+ddR.env <- ddR:::ddR.env
 
 lapply(to_test, test_file)
